f4a4e9317a01f6583456e8656df56c50b2c66bff
Jamie Edwards <jamie_airfix@yahoo.co.uk>
2024-12-14 22:15:02 +0000

    Update Access Token Request Screen Test Tags

--- app/src/androidTest/java/com/sycosoft/allsee/presentation/components/accountaccesspage/AccessTokenRequestScreenTest.kt
+++ app/src/androidTest/java/com/sycosoft/allsee/presentation/components/accountaccesspage/AccessTokenRequestScreenTest.kt
@@ -33,11 +33,11 @@ class AccessTokenRequestScreenTest {
         }
 
         // Then and Verify
-        composeTestRule.onNodeWithTag("atrs_title").isDisplayed()
-        composeTestRule.onNodeWithTag("atrs_text").isDisplayed()
-        composeTestRule.onNodeWithTag("atrs_button").isDisplayed()
-        composeTestRule.onNodeWithTag("otf_access_token").isDisplayed()
-        composeTestRule.onNodeWithTag("atrs_progressbar").isNotDisplayed()
+        composeTestRule.onNodeWithTag(AccessTokenRequestScreenTestTags.TITLE).isDisplayed()
+        composeTestRule.onNodeWithTag(AccessTokenRequestScreenTestTags.TEXT).isDisplayed()
+        composeTestRule.onNodeWithTag(AccessTokenRequestScreenTestTags.BUTTON_GET_STARTED).isDisplayed()
+        composeTestRule.onNodeWithTag(AccessTokenRequestScreenTestTags.ACCESS_TOKEN_INPUT).isDisplayed()
+        composeTestRule.onNodeWithTag(AccessTokenRequestScreenTestTags.PROGRESS_BAR).isNotDisplayed()
     }
 
     @Test
---
+++ app/src/main/java/com/sycosoft/allsee/presentation/components/accountaccesspage/AccessTokenRequestScreenTestTags.kt
@@ -0,0 +1,14 @@
+package com.sycosoft.allsee.presentation.components.accountaccesspage
+
+object AccessTokenRequestScreenTestTags {
+    const val SCREEN = "access_token_request"
+
+    const val BUTTON_GET_STARTED = "button_get_started"
+
+    const val PROGRESS_BAR = "progress_bar"
+
+    const val TITLE = "title"
+    const val TEXT = "text"
+
+    const val ACCESS_TOKEN_INPUT = "access_token_input"
+}
