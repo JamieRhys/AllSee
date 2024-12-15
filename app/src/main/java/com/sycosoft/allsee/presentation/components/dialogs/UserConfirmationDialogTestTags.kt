1c37363b73267014b714ad6347b5b0534e1dec40
Jamie Edwards <jamie_airfix@yahoo.co.uk>
2024-12-15 22:06:27 +0000

    Added user confirmation dialog class

---
+++ app/src/main/java/com/sycosoft/allsee/presentation/components/dialogs/UserConfirmationDialogTestTags.kt
@@ -0,0 +1,8 @@
+package com.sycosoft.allsee.presentation.components.dialogs
+
+object UserConfirmationDialogTestTags {
+    const val TITLE = "user_confirmation_dialog_title"
+    const val TEXT = "user_confirmation_dialog_text"
+    const val CONFIRM_BUTTON = "user_confirmation_dialog_confirm_button"
+    const val DISMISS_BUTTON = "user_confirmation_dialog_dismiss_button"
+}
