332d2e5f5bae3952c12e141bb852de0aff953f28
Jamie Edwards <jamie_airfix@yahoo.co.uk>
2025-02-12 08:55:14 +0000

    Implement Account Details placeholder card.

--- app/src/main/java/com/sycosoft/allsee/presentation/components/cards/accountdetailscard/AccountDetailsCardTestTags.kt
+++ app/src/main/java/com/sycosoft/allsee/presentation/components/cards/accountdetailscard/AccountDetailsCardTestTags.kt
@@ -3,6 +3,7 @@ package com.sycosoft.allsee.presentation.components.cards.accountdetailscard
 object AccountDetailsCardTestTags {
     const val BUTTON_SHARE = "button_share"
     const val TITLE_COUNTRY_NAME = "title_account_type"
+    const val PLACEHOLDER = "placeholder_"
 
     const val BUTTON_COPY_ACCOUNT_HOLDER_NAME = "button_copy_account_holder_name"
     const val TITLE_ACCOUNT_HOLDER_NAME = "title_account_holder_name"
@@ -23,4 +24,7 @@ object AccountDetailsCardTestTags {
     const val BUTTON_COPY_SORT_CODE = "button_copy_sort_code"
     const val TITLE_SORT_CODE = "title_sort_code"
     const val TEXT_SORT_CODE = "text_sort_code"
+
+    const val FLAG_UK = "flag_uk"
+    const val FLAG_INTERNATIONAL = "flag_international"
 }
