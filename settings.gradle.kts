064be2f8831bab92383b76d074a9b362e7b9f7b4
Jamie-Rhys Edwards <jamie_airfix@yahoo.co.uk>
2025-05-07 10:01:59 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AllSee"
+include(":app")
+include(":core")
+include(":features:home")
+include(":features:login")
+include(":features:accountdetails")
